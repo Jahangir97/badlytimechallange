388e5bcbd401d35a0b7d476a066a2ea728c3a963
Jahanda97 <jahangirjn@code.edu.az>
2022-05-05 18:51:32 +0400

    first commit

---
+++ Vehicles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus("Passat", "AxsInfinity", "Blue",123 ,41 );
+
+            Console.WriteLine(bus.Info());
+        }
+    }
+}
